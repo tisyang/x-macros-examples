d0e78886b82b85716b8ec7e583c88f17493f8caf
TyK <tisyang@gmail.com>
2025-11-10 16:07:12 +0800

    Refactor argument parsing macros and functions

--- example_args.c
+++ example_args.c
@@ -1,84 +1,85 @@
+
 #include <stdio.h>
 #include <getopt.h>
 #include <stdlib.h>
 
-#define EMPTY()
-#define POSTPONE(macro_name, args) macro_name EMPTY() ( args )
+#define _ARG_IF_ELSE(CONDITION) _ARG_IF_ELSE_##CONDITION
+#define _ARG_IF_ELSE_0(THEN, ELSE) ELSE
+#define _ARG_IF_ELSE_1(THEN, else) THEN
 
-#define _IF_ELSE(condition) _IF_ELSE_##condition
-#define _IF_ELSE_0(then, else) else
-#define _IF_ELSE_1(then, else) then
+#define _ARG_DEFINE_FIELD(TYPE, NAME, SHORT, NEED, DEF, TIPS) \
+        TYPE    NAME;
+
+#define _ARG_DEFINE_DEFAULT(TYPE, NAME, SHORT, NEED, DEF, TIPS) \
+        DEF,
+
+#define _ARG_STRUCT_NAME(LIST)  st_##LIST##_name
+
+#define ARG_STRUCT_TYPE(LIST) \
+    struct _ARG_STRUCT_NAME(LIST) { \
+        LIST(_ARG_DEFINE_FIELD) \
+    }
+
+#define _ARG_STRUCT_INITIALIZER(LIST) \
+    { LIST(_ARG_DEFINE_DEFAULT) }
+
+#define _ARG_GETOPT_SHORT(TYPE, NAME, SHORT, NEED, DEF, TIPS) \
+    _ARG_IF_ELSE(NEED)(#SHORT ":", #SHORT)
+
+
+#define _ARG_CONCAT_IMPL(x, y, z) x##y##z
+#define _ARG_CONCAT(x, y, z) _ARG_CONCAT_IMPL(x, y, z)
+#define _ARG_SQUOTE '
+#define _ARG_CHARIFY(c)  _ARG_CONCAT(_ARG_SQUOTE, c, _ARG_SQUOTE)
+
+#define _ARG_CONV_int    atoi
+#define _ARG_CONV_double atof
+#define _ARG_CONV_str(x) x
+
+#define _ARG_CASE(TYPE, NAME, SHORT, NEED, DEF, TIPS) \
+    case _ARG_CHARIFY(SHORT): _ARG_IF_ELSE(NEED)(_args.NAME = _ARG_CONV_##TYPE(optarg), _args.NAME = 1); break;
+
+#define _ARG_PARSE_FUNC_NAME(LIST)  _arg_parse_##LIST
+
+#define ARG_PARSE_DEFINE(LIST) \
+    static ARG_STRUCT_TYPE(LIST) * _ARG_PARSE_FUNC_NAME(LIST)(int argc, char *argv[]) \
+    { \
+        static struct _ARG_STRUCT_NAME(LIST) _args = _ARG_STRUCT_INITIALIZER(LIST); \
+        int c; \
+        while ((c = getopt(argc, argv, LIST(_ARG_GETOPT_SHORT))) != -1) { \
+            switch (c) { \
+            LIST(_ARG_CASE) \
+            } \
+        } \
+        return &_args; \
+    }
+
+#define ARG_PARSE(LIST, ...)    _ARG_PARSE_FUNC_NAME(LIST)(__VA_ARGS__)
+
+#define _ARG_PRINT(TYPE, NAME, SHORT, NEED, DEF, TIPS) \
+    printf("arg %s=%s: ", #SHORT, #NAME); \
+    printf(_Generic((TYPE){0}, int: "%d", char*: "%s", double: "%lf"), p_arg->NAME); \
+    printf(" (%s)\n", TIPS);
+
+#define ARG_STRUCT_PRINT(LIST, STRUCT_P) \
+    do { \
+        struct _ARG_STRUCT_NAME(LIST) *p_arg = STRUCT_P; \
+        LIST(_ARG_PRINT) \
+    } while (0)
+
+
+// useage:
 
 #define APP_ARGS_LIST(X) \
     X(int,      help,    h, 0,   0,      "show help") \
     X(int,      verbose, v, 1,   0,      "verbose level") \
     X(double,   timeout, t, 1,   0.0,    "timeout")
 
-
-#define _STRUCT_FIELD(TYPE, NAME, SHORT, NEED, DEF, TIPS) \
-        TYPE    NAME;
-
-#define _STRUCT_DEFAULT(TYPE, NAME, SHORT, NEED, DEF, TIPS) \
-        DEF,
-
-#define LIST_ST_NAME(LIST)  st_##LIST##_name
-
-#define APP_ARGS_STRUCT(LIST) \
-    struct LIST_ST_NAME(LIST) { \
-        LIST(_STRUCT_FIELD) \
-    }
-
-#define APP_ARGS_STRUCT_INITIALIZER(LIST) \
-    { LIST(_STRUCT_DEFAULT) }
-
-#define _OPTARGS(TYPE, NAME, SHORT, NEED, DEF, TIPS) \
-    _IF_ELSE(NEED)(#SHORT ":", #SHORT)
-
-
-#define CONCAT_IMPL(x, y, z) x##y##z
-#define CONCAT(x, y, z) CONCAT_IMPL(x, y, z)
-#define QUOTE '
-#define CHARIFY(c)  CONCAT(QUOTE, c, QUOTE)
-
-#define CONV_int    atoi
-#define CONV_double atof
-#define CONV_str(x) x
-
-#define _STRUCT_OPT(TYPE, NAME, SHORT, NEED, DEF, TIPS) \
-    case CHARIFY(SHORT): _IF_ELSE(NEED)(_args.NAME = CONV_##TYPE(optarg), _args.NAME = 1); break;
-
-#define LIST_FUNC_NAME(LIST)  app_args_parse_##LIST
-#define APP_ARGS_PARSE(LIST) \
-    static APP_ARGS_STRUCT(LIST) * LIST_FUNC_NAME(LIST)(int argc, char *argv[]) \
-    { \
-        static struct LIST_ST_NAME(LIST) _args = APP_ARGS_STRUCT_INITIALIZER(LIST); \
-        int c; \
-        while ((c = getopt(argc, argv, LIST(_OPTARGS))) != -1) { \
-            switch (c) { \
-            LIST(_STRUCT_OPT) \
-            } \
-        } \
-        return &_args; \
-    }
-
-#define LIST_PARSE(LIST, ...)    LIST_FUNC_NAME(LIST)(__VA_ARGS__)
-
-#define _STRUCT_PRINT(TYPE, NAME, SHORT, NEED, DEF, TIPS) \
-    printf("arg %s=%s: ", #SHORT, #NAME); \
-    printf(_Generic((TYPE){0}, int: "%d", char*: "%s", double: "%lf"), p_arg->NAME); \
-    printf(" (%s)\n", TIPS);
-
-#define PRINT_LIST_ST(LIST, STRUCT_P) \
-    do { \
-        struct LIST_ST_NAME(LIST) *p_arg = STRUCT_P; \
-        LIST(_STRUCT_PRINT) \
-    } while (0)
-
-APP_ARGS_PARSE(APP_ARGS_LIST);
+ARG_PARSE_DEFINE(APP_ARGS_LIST);
 
 int main(int argc, char **argv)
 {
-    APP_ARGS_STRUCT(APP_ARGS_LIST) *arg = LIST_PARSE(APP_ARGS_LIST, argc, argv);
-    PRINT_LIST_ST(APP_ARGS_LIST, arg);
+    ARG_STRUCT_TYPE(APP_ARGS_LIST) *arg = ARG_PARSE(APP_ARGS_LIST, argc, argv);
+    ARG_STRUCT_PRINT(APP_ARGS_LIST, arg);
     return 0;
 }
